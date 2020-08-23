1251c5861215f1010ef7f7e172324059559528ea
Orla Madden <omadden2@gmail.com>
2020-08-23 07:26:47 +0000

    customised how the products and categories are shown in the admin dashboard

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
-admin.site.register(Category)
+""" 
+Customises what fields to display in
+the Product admin dashboard
+"""
+class ProductAdmin(admin.ModelAdmin):
+    # what fields to display
+    list_display = (
+        'name',
+        'category',
+        'price',
+    )
+    
+    # orders inventory a-z by name
+    ordering = ('name',)
+
+""" 
+Customises what fields to display in
+the Category admin dashboard
+"""
+class CategoryAdmin(admin.ModelAdmin):
+    # what fields to display
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
