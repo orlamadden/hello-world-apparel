bc23df8ce6dbc6ae38b3b8070e36a78560e4fb68
Orla Madden <omadden2@gmail.com>
2020-08-24 06:23:19 +0000

    edited default site header and site title in django admin area

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# changes header in Django admin area
+admin.site.site_header = 'Hello World Apparel Admin'
+
+# changes site title of Django admin dashboard
+admin.site.index_title = 'Welcome to the Hello World Apparel admin area'
