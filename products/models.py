b86824cf926f53863981df9b8c05f1ca5cf18516
Orla Madden <omadden2@gmail.com>
2020-09-01 05:04:27 +0000

    included size functionality to the model for apparel

--- products/models.py
+++ products/models.py
@@ -20,6 +20,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
