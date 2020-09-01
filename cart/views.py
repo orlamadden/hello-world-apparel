fdddfb479eaea6266e9f49579dd84d5a369cb37e
Orla Madden <omadden2@gmail.com>
2020-09-01 06:12:15 +0000

    added size and product logic to cart

--- cart/views.py
+++ cart/views.py
@@ -11,11 +11,23 @@ def add_to_cart(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
+    if size:
+        if item_id in list(cart.keys()):
+            if size in cart[item_id]['items_by_size'].keys():
+                cart[item_id]['items_by_size'][size] += quantity
+            else:
+                cart[item_id]['items_by_size'][size] = quantity
+        else:
+            cart[item_id] = {'items_by_size': {size: quantity}}
     else:
-        cart[item_id] = quantity
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
 
     request.session['cart'] = cart
     print( request.session['cart'])
