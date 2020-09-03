58d8a5c94f71a12ff41de7ab7c1b7b85f5d6dcb9
Orla Madden <omadden2@gmail.com>
2020-09-03 07:07:27 +0000

    added checkout url to project level app

--- hello_world_apparel/urls.py
+++ hello_world_apparel/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
-    path('cart/', include('cart.urls'))
+    path('cart/', include('cart.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
