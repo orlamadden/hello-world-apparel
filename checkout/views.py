01f70fd66cd348fe0806a6d3098161fa935932c2
Orla Madden <omadden2@gmail.com>
2020-09-03 07:04:28 +0000

    created checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your shoppping cart at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
