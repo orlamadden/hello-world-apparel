2968f07b750847d05ea46258eba57dc620e2004f
Orla Madden <omadden2@gmail.com>
2020-09-13 11:47:09 +0000

    created product form, views and urls for super users to add products

--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
 
+from .forms import ProductForm
+
 def all_products(request):
     """ 
     A view to show all products, 
@@ -70,3 +72,25 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+    
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
