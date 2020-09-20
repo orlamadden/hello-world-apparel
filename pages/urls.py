6dbd1bf3266161aba7331d13c65387d91dc73a9f
Orla Madden <omadden2@gmail.com>
2020-09-20 20:00:25 +0000

    added terms and conditions page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import privacy_policy_view
+from .views import privacy_policy_view, terms_and_conditions_view
 
 urlpatterns = [
     path('privacy_policy/', privacy_policy_view, name="privacy_policy"),
+    path('terms_and_conditions/', terms_and_conditions_view, name="terms_and_conditions"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,10 @@ def privacy_policy_view(request, *args, **kwargs):
     Renders view for the privacy policy page
     """
     return render(request, "privacy_policy.html")
+
+
+def terms_and_conditions_view(request, *args, **kwargs):
+    """
+    Renders view for the privacy policy page
+    """
+    return render(request, "terms_and_conditions.html")
