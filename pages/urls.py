f3943ac357ec9efb762188d3e3379c9002566cb4
Orla Madden <omadden2@gmail.com>
2020-09-20 20:25:48 +0000

    created about us page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import privacy_policy_view, terms_and_conditions_view
+from .views import privacy_policy_view, terms_and_conditions_view, about_us_view
 
 urlpatterns = [
     path('privacy_policy/', privacy_policy_view, name="privacy_policy"),
     path('terms_and_conditions/', terms_and_conditions_view, name="terms_and_conditions"),
+    path('about_us/', about_us_view, name="about_us"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+def about_us_view(request, *args, **kwargs):
+    """
+    Renders view for the about us page
+    """
+    return render(request, "about_us.html")
+
 
 def privacy_policy_view(request, *args, **kwargs):
     """
@@ -10,6 +16,6 @@ def privacy_policy_view(request, *args, **kwargs):
 
 def terms_and_conditions_view(request, *args, **kwargs):
     """
-    Renders view for the privacy policy page
+    Renders view for the terms and conditions page
     """
     return render(request, "terms_and_conditions.html")
