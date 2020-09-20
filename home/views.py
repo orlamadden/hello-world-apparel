7ea621769428da8eb102ecc3bba8e718fdf4327c
Orla Madden <omadden2@gmail.com>
2020-09-20 21:32:12 +0000

    added featured products to home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """ homepage view """
-    return render(request, 'home/index.html')
+    
+    featured_products = Product.objects.filter(featured=True).order_by('?')[:6]
+
+    context = {
+        'featured_listings': featured_products,
+    }
+
+    return render(request, 'home/index.html', context)
