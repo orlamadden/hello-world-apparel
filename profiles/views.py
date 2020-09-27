01d4e8b3e64e3b3a9cedc5e34cd2edfb2ab59280
Orla Madden <omadden2@gmail.com>
2020-09-27 18:35:32 +0000

    code clean up and comments added

--- profiles/views.py
+++ profiles/views.py
@@ -10,7 +10,9 @@ from checkout.models import Order
 
 @login_required
 def profile(request):
-    """ Display the user's profile. """
+    """
+    Display the user's profile
+    """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -35,6 +37,9 @@ def profile(request):
 
 
 def order_history(request, order_number):
+    """
+    Display user's order history
+    """
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
